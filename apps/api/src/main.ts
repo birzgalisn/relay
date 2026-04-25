9bd118b9119fa3d7a5733094c774eab2f6e0fc0b
Niks Birzgalis <hello@niksbirzgalis.com>
2026-04-25 08:06:23 +0000

    feat: use fastify instead of express

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,15 @@
-import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
-
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import 'reflect-metadata';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
   app.enableCors();
   app.enableShutdownHooks();
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 
 void bootstrap();
